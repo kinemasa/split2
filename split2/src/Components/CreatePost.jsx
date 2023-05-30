10983356c1acf1d405e854103fbc80cd64dfb770
Masaya Kinefuchi <kine0718@icloud.com>
2023-05-30 21:55:03 +0900

    create post

--- split2/src/Components/CreatePost.jsx
+++ split2/src/Components/CreatePost.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from 'react';
+import {useState,useEffect} from "react"
+import {addDoc,collection} from "firebase/firestore"
+import {db, auth} from "../firebase"
+import {useNavigate} from "react-router-dom";
 
-const CreatePost = () => {
-  return (
-    <div>CreatePost</div>
-  )
+
+
+const CreatePost = (isAuth) => {
+
+  const navigate = useNavigate();
+
+  const [title,setTitle] = useState();
+  const [postText,setPostText]  =useState();
+
+  const createPost = async() =>{
+    await addDoc(collection(db,"posts"),{
+      title:title,
+      postsText:postText,
+      username:auth.currentUser.displayName
+    });
+    navigate("/")
+  }
+  useEffect(() =>{
+if(!isAuth){
+  navigate("/login");
 }
+  },[])
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="h-2/3 w-3/4 shadow-lg flex flex-col items-center md:w-1/2 md:h-1/2">
+
+        <h1 className="text-3xl">記事を投稿する</h1>
+        <p className ="text-2xl p-6">申請内容</p>
+        <input type ="text" className="w-full p-4 border-4" onChange={(e) => setTitle(e.target.value)}></input>
+        <p className ="text-2xl p-6">金額</p>
+        <div className='flex justify-center text-center p-4'>
+        <input type ="text" className="w-1/2 h-full p-3 border-4"  onChange={(e) => setPostText(e.target.value)}></input>
+        <div className='p-4 text-2xl'>円</div>
+        </div>
+          
+          <button className='w-40 h-20  m-2 shadow-lg bg-blue-400 rounded-lg hover:bg-blue-500'onClick={createPost}>投稿</button>
+        </div>
+        
+      </div>
+   
+  );
+};
 
 export default CreatePost
--- split2/src/Components/Home.jsx
+++ split2/src/Components/Home.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
+import { getDocs, collection, doc, deleteDoc } from "firebase/firestore";
+import { db, auth } from "../firebase";
+
+
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [postList, setPostList] = useState([]);
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, []);
 
-export default Home
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "posts", id));
+    window.location.href = "/";
+  };
+  return (
+    <div className="homePage">
+      {postList.map((post) => {
+        return (
+          <div className="postContents" key={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+
+            <div className="postTextContainer">{post.postsText}</div>
+            <div className="nameAndDeleteButton">
+              <h3>{post.username}</h3>
+              {post.username === auth.currentUser.displayName && (
+                <button onClick={() => handleDelete(post.id)}>削除</button>
+              )}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Home;
